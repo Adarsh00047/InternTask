b295a0f3d7464f186209d7227adee1f4ea699bd9
Adarsh047 <adarsh@madstreetden.com>
2024-01-09 15:14:02 +0530

    added api documentation

--- app/api.py
+++ app/api.py
@@ -1,3 +1,4 @@
+# Import the necessary libraries
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 from app.ocr import perform_ocr
@@ -7,8 +8,20 @@ from timeit import default_timer as timer
 import tempfile
 from pdf2image import convert_from_path
 import shutil
+from app.face import classify_side
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "upload",
+        "description": "Post your document to this endpoint to perform classification",
+    }
+]
+
+
+app = FastAPI(title="Document Classifier",
+              summary="Document Classifier for Aadhar, Pan Cards, Passport and Voter ID using Google OCR",
+              version="1.0",
+              openapi_tags=tags_metadata)
 
 def classify_text(text, doc_keywords):
     matching_types = []
@@ -50,63 +63,7 @@ def classify_document_side(document_type, text, doc_keywords, side):
         classified_side.append("Not identified")
         return classified_side
 
-
-# @app.post("/upload/image")
-# async def upload_image(file: UploadFile = File(...)):
-#     doc_keywords = {"aadhar": ["government of india", "unique identification authority", "uidai", "help@uidai.gov.in"],
-#                 "pan": ["income tax department", "permanent account number", "income tax pan services unit"],
-#                 "passport": ["nationality", "republic of india", "country code"],
-#                 "voter": ["election commission of india", "epic", "elector", "elector's", "electoral registration officer"]}
-#     side = {
-#     "voter": 
-#     {
-#         "front": ["election commission of india", "elector"],
-#         "back": ["electoral registration officer"]
-#     },
-#     "aadhar":
-#     {
-#         "front": ["government of india"],
-#         "back": ["unique identification authority"]
-#     },
-#     "pan":
-#     {
-#         "front": ["income tax department"],
-#         "back": ["income tax pan services unit", "nsdl"]
-#     },
-#     "passport":
-#     {
-#         "front": ["nationality", "republic of india"],
-#         "back": [""]
-#     }}
-#     try:
-#         # Save the uploaded image temporarily
-#         with open(file.filename, "wb") as image_file:
-#             image_file.write(file.file.read())
-
-#         # Perform OCR on the image
-#         ocr_text = perform_ocr(file.filename)
-#         print(ocr_text)
-
-#         # Classify document type based on OCR text
-#         document_type = classify_text(ocr_text, doc_keywords=doc_keywords)
-
-#         # Classify document side based on OCR text and document type
-#         response = {}
-#         for doc in document_type:
-#             doc_side = classify_document_side(doc, ocr_text, doc_keywords, side)
-#             try:
-#                 response[doc].extend(doc_side)
-#             except:
-#                 response[doc] = doc_side
-
-#         # return JSONResponse(content={"document_type": document_type, "document_side": document_side})
-#         return JSONResponse(content=response)
-#     finally:
-#         # Remove the temporary image file
-#         import os
-#         os.remove(file.filename)
-
-@app.post("/upload/images")
+@app.post("/upload/images", tags=["upload"])
 async def upload_images(files: List[UploadFile] = File(...)):
     doc_keywords = {
         "aadhar": ["government of india", "unique identification authority", "uidai", "help@uidai.gov.in"],
@@ -159,7 +116,10 @@ async def upload_images(files: List[UploadFile] = File(...)):
                     # Classify document side based on OCR text and document type
                     classification_results = {}
                     for doc_type in document_types:
-                        doc_side = classify_document_side(doc_type, ocr_text, doc_keywords, side)
+                        if doc_type == "aadhar" and len(document_types) == 1:
+                            doc_side = classify_side(image_file)
+                        else:
+                            doc_side = classify_document_side(doc_type, ocr_text, doc_keywords, side)
                         classification_results[doc_type] = doc_side
 
                     # Store the classification results in the response dictionary with the filename as the key
@@ -196,7 +156,10 @@ async def upload_images(files: List[UploadFile] = File(...)):
                 # Classify document side based on OCR text and document type
                 classification_results = {}
                 for doc_type in document_types:
-                    doc_side = classify_document_side(doc_type, ocr_text, doc_keywords, side)
+                    if doc_type == "aadhar" and len(document_types) == 1:
+                        doc_side = classify_side(file.filename)
+                    else:
+                        doc_side = classify_document_side(doc_type, ocr_text, doc_keywords, side)
                     classification_results[doc_type] = doc_side
 
                 # Store the classification results in the response dictionary with the filename as the key
